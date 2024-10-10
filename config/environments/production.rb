b8b329169bfab93fa17d74ec34eff623c108daff
Claudiney Veloso <claudineyveloso@gmail.com>
2024-10-10 17:10:12 -0300

    update environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  # Adiciona hosts permitidos aqui
+  config.hosts << "myr-quiz.esgsolutions.com.br"
+  config.hosts << "52.207.69.202" # Caso você esteja usando um IP
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
