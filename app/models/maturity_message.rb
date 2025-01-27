546c55980c881df0e2ba0d3ef19ee827bce7efdf
Claudiney Veloso <claudineyveloso@gmail.com>
2025-01-27 10:36:52 -0300

    Fix text field with null values

--- app/models/maturity_message.rb
+++ app/models/maturity_message.rb
@@ -2,10 +2,16 @@ class MaturityMessage < ApplicationRecord
   belongs_to :maturity
   belongs_to :axi
 
+  before_save :set_default_message
+
   validates :maturity_id,
             :axi_id,
             presence: true
 
   validates :message,
             presence: true
+
+  def set_default_message
+    self.message ||= ""
+  end
 end
