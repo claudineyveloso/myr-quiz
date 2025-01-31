1333754ad4746622769a0d9937b93b47aa5f5dd5
Claudiney Veloso <claudineyveloso@gmail.com>
2025-01-30 22:20:16 -0300

    Remove pid file from server if exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Remove o arquivo pid do servidor caso exista
+rm -f /app/tmp/pids/server.pid
+
 # Executando db:create e db:migrate
 echo "Executando db:create..."
 bundle exec rails db:create
