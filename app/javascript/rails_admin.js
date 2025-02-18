f32402728bfac67b8cceb52a277e9014f066174d
Claudiney Veloso <claudineyveloso@gmail.com>
2025-02-18 16:34:26 -0300

    added js chart for the client graph displayed in rails admin

--- app/javascript/rails_admin.js
+++ app/javascript/rails_admin.js
@@ -1,1 +1,10 @@
 import "rails_admin/src/rails_admin/base";
+RailsAdmin.on("rails_admin.dom.ready", function () {
+  const script1 = document.createElement("script");
+  script1.src = "https://cdn.jsdelivr.net/npm/chart.js";
+  document.body.appendChild(script1);
+
+  const script2 = document.createElement("script");
+  script2.src = "https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels";
+  document.body.appendChild(script2);
+});
