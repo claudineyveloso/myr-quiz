d2b0ebd54bd303d4555ed5f3421c41239bfddb90
Claudiney Veloso <claudineyveloso@gmail.com>
2025-02-21 10:05:26 -0300

    remembered

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@rails/ujs";
 import "@hotwired/turbo-rails";
 import "controllers";
 import * as bootstrap from "bootstrap";
-import "rails_admin";
+//import "rails_admin";
+//import "jquery-ui";
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,7 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js"
 pin "sweetalert2", to: "https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"
+#bin/pin "rails_admin", preload: true
+#pin "rails_admin/src/rails_admin/base", to: "https://ga.jspm.io/npm:rails_admin@3.2.0/src/rails_admin/base.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@6.1.7/lib/assets/compiled/rails-ujs.js"
+#pin "jquery-ui", to: "https://ga.jspm.io/npm:jquery-ui@1.13.3/dist/jquery-ui.js"
