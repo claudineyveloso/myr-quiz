b5330e528d97c38afa2fd52a7692ad8737fc46c5
Claudiney Veloso <claudineyveloso@gmail.com>
2025-02-21 10:29:31 -0300

    add

--- config/application.rb
+++ config/application.rb
@@ -14,9 +14,10 @@ module MyrQuiz
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    # config.autoload_lib(ignore: %w[assets tasks])
     config.autoload_paths << Rails.root.join('lib')
     config.eager_load_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib', 'rails_admin')
 
     config.time_zone = "Brasilia"
     # config.active_record.default_timezone = 'Brasilia'
